670d3014169965ac32fb2352d5e6958f7ca85220
Caio Freitas <caio078freitas@gmail.com>
2025-11-10 22:37:55 -0300

    chore: configuração inicial do FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from gestor-estudio-arte!")
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+
+load_dotenv()
+origin = os.getenv("FRONT_URL")
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origin],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Olá"}
