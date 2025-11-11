525136f235928f7a016081aa5f6cf94d5690be7a
Caio Freitas <caio078freitas@gmail.com>
2025-11-11 00:49:34 -0300

    chore(frontend): configuração das rotas e criação do template das páginas principais

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet, Link } from "react-router";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="app-layout">
+        {/* Um menu de navegação simples */}
+        <nav style={{ padding: "1rem", backgroundColor: "#333" }}>
+          <Link to="/" style={{ marginRight: "1rem" }}>
+            Dashboard
+          </Link>
+          <Link to="/clientes">Clientes</Link>
+          {/* /produtos aqui quando a US-07 for iniciada */}
+        </nav>
+
+        <hr />
+
+        {/* O 'Outlet' é onde o react-router irá renderizar a página da rota atual */}
+        <main style={{ padding: "1rem" }}>
+          <Outlet />
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/ClientsPage.jsx
@@ -0,0 +1,12 @@
+function ClientsPage() {
+  return (
+    <>
+      <div>
+        <h1>Gestão de Clientes</h1>
+        <p>Esta tela irá listar e cadastrar clientes.</p>
+      </div>
+    </>
+  );
+}
+
+export default ClientsPage;
