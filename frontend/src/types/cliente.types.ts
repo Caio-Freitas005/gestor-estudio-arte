83d3115ec34c86ba9fe7c4181e4177ab3a1d8bb6
Caio Freitas <caio078freitas@gmail.com>
2025-11-16 16:51:45 -0300

    refactor(frontend): torna os campos opcionais e utiliza herança para evitar repetição

--- frontend/src/types/cliente.types.ts
+++ frontend/src/types/cliente.types.ts
@@ -3,10 +3,10 @@
 
 interface ClienteBase {
   nm_cliente: string;
-  cd_telefone: string | null;
-  nm_email: string | null;
-  dt_nascimento: string | null; // O <input type="date"> do HTML envia strings
-  ds_observacoes: string | null;
+  cd_telefone?: string | null;
+  nm_email?: string | null;
+  dt_nascimento?: string | null; // O <input type="date"> do HTML envia strings
+  ds_observacoes?: string | null;
 }
 
 export interface ClienteCreate extends ClienteBase {
@@ -17,10 +17,7 @@ export interface ClientePublic extends ClienteBase {
   cd_cliente: number;
 }
 
-export interface ClienteUpdate {
-  nm_cliente?: string; // O ? significa que o campo é opcional
-  cd_telefone?: string | null;
-  nm_email?: string | null;
-  dt_nascimento?: string | null;
-  ds_observacoes?: string | null;
+// Partial torna todos os campos opcionais
+export interface ClienteUpdate extends Partial<ClienteBase> {
+  //
 }
