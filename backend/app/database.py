5a6a3d9b3ef3e4925dc12b46b052bfce5a5975c7
Caio Freitas <caio078freitas@gmail.com>
2025-11-30 00:08:42 -0300

    chore(backend): fixa o caminho do arquivo do banco de dados

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,16 @@
+from pathlib import Path
 from typing import Annotated
 
 from fastapi import Depends
 from sqlmodel import Session, SQLModel, create_engine
 
+# Define o caminho do arquivo do banco
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_DIR = BASE_DIR
+
 db_name = "gestor.db"
-db_url = f"sqlite:///{db_name}"
+db_path = DB_DIR / db_name
+db_url = f"sqlite:///{db_path}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(db_url, echo=True, connect_args=connect_args)
