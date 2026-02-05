577411b430a43eda42bff3848014237a525cf96f
Caio Freitas <caio078freitas@gmail.com>
2026-02-05 20:05:58 -0300

    fix: corrige erro de exibição do dia atual ao editar um pedido

--- frontend/src/utils/format.utils.ts
+++ frontend/src/utils/format.utils.ts
@@ -6,25 +6,30 @@ export function formatPhone(phone: string | undefined): string {
     return `(${match[1]}) ${match[2]}-${match[3]}`;
   }
   return phone;
-};
+}
 
 export function formatDate(date: string | undefined): string {
   return new Date(date).toLocaleDateString("pt-BR", {
     timeZone: "UTC",
   });
-};
+}
 
-export function formatDateForInput (dateInput?: string | Date) {
-  const d = dateInput ? new Date(dateInput) : new Date();
+export function formatDateForInput(dateInput?: string | Date) {
+  // Editar (string vinda do Banco de Dados)
+  // Retorna a string pura YYYY-MM-DD para o browser não tentar ajustar o fuso.
+  if (typeof dateInput === "string") {
+    return dateInput.split("T")[0];
+  }
 
-  // Extrai os componentes locais 
+  // Se for Novo Pedido (undefined) ou um objeto Date, usa o relógio LOCAL
+  const d = dateInput || new Date();
   const year = d.getFullYear();
-  const month = String(d.getMonth() + 1).padStart(2, "0"); // Meses começam em 0
+  const month = String(d.getMonth() + 1).padStart(2, "0");
   const day = String(d.getDate()).padStart(2, "0");
 
-  return `${year}-${month}-${day}`; // Retorna exatamente AAAA-MM-DD
-};
+  return `${year}-${month}-${day}`;
+}
 
 export function formatNumber(number: number | undefined): any {
   return Number(number).toFixed(2);
-};
+}
