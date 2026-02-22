6a552a61958352e8c2b35292497a0c74a9fb27e1
Caio Freitas <caio078freitas@gmail.com>
2026-02-22 14:12:03 -0300

    feat(backend): implementa o tipo PhoneNumber do Pydantic no campo telefone, sobrescrevendo com a classe TelefoneBR para dar suporte ao padrão brasileiro

--- backend/app/models/cliente.py
+++ backend/app/models/cliente.py
@@ -1,6 +1,7 @@
 from datetime import date
 from typing import TYPE_CHECKING
 
+from pydantic_extra_types.phone_numbers import PhoneNumber
 from sqlmodel import Field, Relationship, SQLModel  # type: ignore
 
 from .base import TimestampMixin
@@ -8,10 +9,15 @@ from .base import TimestampMixin
 if TYPE_CHECKING:
     from .pedido import Pedido
 
+# Adapta telefone para o tipo brasileiro
+class TelefoneBR(PhoneNumber):
+    default_region_code = "BR"
+    # E164 salva como: +5511999999999 
+    phone_format = "E164"
 
 class ClienteBase(TimestampMixin, SQLModel):
     nome: str = Field(index=True)
-    telefone: str | None = Field(default=None, max_length=11, index=True)
+    telefone: TelefoneBR | None = Field(default=None, max_length=20, index=True)
     email: str | None = Field(default=None, max_length=100, unique=True)
     data_nascimento: date | None = None
     observacoes: str | None = None
@@ -38,7 +44,7 @@ class ClientePublicPaginated(SQLModel):
 
 class ClienteUpdate(SQLModel):
     nome: str | None = None
-    telefone: str | None = Field(default=None, max_length=11)
+    telefone: TelefoneBR | None = Field(default=None, max_length=20)
     email: str | None = Field(default=None, max_length=100)
     data_nascimento: date | None = None
     observacoes: str | None = None
