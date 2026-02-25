02190fc33cff609523a84e37ace417343b059749
Caio Freitas <caio078freitas@gmail.com>
2026-02-25 02:44:43 -0300

    chore(frontend): retira import inutilizado

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import {
   createTheme,
   ThemeProvider,
