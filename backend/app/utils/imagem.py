697912dc0f11cead1f538f4a6d692a7b60cf7ef1
Caio Freitas <caio078freitas@gmail.com>
2026-02-26 20:12:22 -0300

    feat(backend): implemente validação de tamanho das imagens

--- backend/app/utils/imagem.py
+++ backend/app/utils/imagem.py
@@ -7,10 +7,22 @@ from PIL import Image, ImageOps
 
 from ..config import ARTES_DIR
 
+MAX_FILE_SIZE = 15 * 1024 * 1024  # 15 MB
+
 
 def process_art_image(file: UploadFile, pedido_id: int, produto_id: int) -> str:
-    """Processa e salva imagem em disco, garantindo que tenha formatos permitidos"""
+    """Processa e salva imagem em disco, garantindo que tenha formatos permitidos e esteja dentro do tamanho máximo"""
     try:
+        # Pula para o final do arquivo para ver o tamanho
+        file.file.seek(0, 2)
+        file_size = file.file.tell()
+        file.file.seek(0)  # Volta o cursor para o início para o PIL conseguir ler
+
+        if file_size > MAX_FILE_SIZE:
+            raise HTTPException(
+                status_code=400, detail="A imagem deve ter no máximo 15MB."
+            )
+
         content = file.file.read()
         img = Image.open(io.BytesIO(content))
         if img.format not in ["PNG", "JPG", "JPEG", "WEBP"]:
